20836f3077de0a41f9241354cb85718dcb98ce7d
Lucas Lemos Ortega <34188388+LoukasOrtyx@users.noreply.github.com>
2021-11-14 12:22:04 -0300

    Add files via upload

---
+++ myBitReversalOrtega.m
@@ -0,0 +1,10 @@
+function X = myBitReversalOrtega(x)
+  X = zeros(1, length(x));
+  num_bits = log2(length(x));
+  assert(floor(num_bits) == num_bits, 'o tamanho do vetor de entrada x não é uma potência de 2');
+  for i = 0 : length(x) - 1
+    rev_bin = flip(dec2bin(i, num_bits));
+    num = bin2dec(rev_bin) + 1;
+    X(num) = x(i + 1);
+  endfor
+endfunction
---
+++ myfftOrtega.m
@@ -0,0 +1,22 @@
+function X = myfftOrtega(x)
+  stages = log2(length(x));         # Quantidade de estágios da FFT
+  X = myBitReversalOrtega(x);       # Sinal de saída
+  W = e ^ -(2j * pi / length(x));   # Fator de Giro
+  for stage = 1 : stages
+    n_pts = 2^stage;                # Quantidade de pontos em cada bloco da FFT
+    half = n_pts / 2;                 
+    temp = X;
+    count = 1;                      # Variável que percorre os pontos da FFT
+    while count < length(x)    
+      # k percorre metade de cada bloco e garante que soma e subtração preencham
+      # o n-ésimo elemento de X e seu correspondente n-ésimo + length(x) / 2
+      for k = 0 : half - 1          
+        m = (k * length(x) / (2 ^ stage));    # Expoente do fator de giro.
+        X(count) = temp(count) + W ^ m * temp(count + half);
+        X(count + half) = temp(count) - W ^ m * temp(count + half);
+        count += 1;
+      endfor
+      count += half;
+    endwhile
+  endfor
+endfunction
